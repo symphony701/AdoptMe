d799bc65b7b16d84b80792cc79e2ee775ef7cbe4
dylandSteven <stevendylandsaldana@gmail.com>
2021-11-27 14:32:53 -0500

    Adding Ubications controllers,module,service,entity,Dto

--- src/modules/ubications/ubications.controller.ts
+++ src/modules/ubications/ubications.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { UbicationsService } from "./ubications.service";
+import { CreateUbicationsDto } from "./dtos/CreateUbicationDto";
 
 @Controller('ubications')
-export class UbicationsController {}
+export class UbicationsController {
+  constructor(private readonly ubicationsService:UbicationsService)
+  {
+
+  }
+
+  @Get()
+  findAll()
+  {
+    return this.ubicationsService.findAll();
+  }
+
+  @Get(':id')
+  find(@Param('id')id:number)
+  {
+    return this.ubicationsService.find(id);
+  }
+
+  @Post()
+  insert(@Body() createUbicationsDto:CreateUbicationsDto)
+  {
+    return this.ubicationsService.insert(createUbicationsDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id')id:number)
+  {
+    return this.ubicationsService.delete(id);
+  }
+
+  @Put(':id')
+  update(@Param('id')id:number,
+         @Body()createUbicationDto:CreateUbicationsDto,)
+  {
+    return this.ubicationsService.update(id,createUbicationDto);
+  }
+
+
+
+}
--- src/modules/ubications/ubications.module.ts
+++ src/modules/ubications/ubications.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UbicationsController } from './ubications.controller';
 import { UbicationsService } from './ubications.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Ubications } from "./entities/UbicationEntity";
 
 @Module({
   controllers: [UbicationsController],
-  providers: [UbicationsService]
+  providers: [UbicationsService],
+  imports:[TypeOrmModule.forFeature([Ubications])],
 })
 export class UbicationsModule {}
